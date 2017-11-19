c66c6f7e7cdf4f2103e8faae7b6f176e11d8d79f
Viktor Salanki <salankiv5@gmail.com>
2017-11-19 18:38:05 +0100

    init project

---
+++ src/main/java/com/salankiv/cicmiscanner/CicmiscannerApplication.java
@@ -0,0 +1,12 @@
+package com.salankiv.cicmiscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicmiscannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicmiscannerApplication.class, args);
+	}
+}
